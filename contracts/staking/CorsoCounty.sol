7dd9b9e9b695968fb7057001e809c901b5b5110e
CorsoCounty <cane@corsocounty.xyz>
2021-06-29 18:08:08 +0100

    change of direction

--- contracts/staking/CorsoCounty.sol
+++ contracts/staking/CorsoCounty.sol
@@ -4,9 +4,9 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./NFTMinter.sol";
 
-contract CorsoStaking is Ownable, NFTMinter {
+
+contract CorsoStaking is Ownable {
     using SafeERC20 for IERC20;
 
     struct UserInfo {
